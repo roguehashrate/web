9e673fe578c3ee46d7f1873ad07acf9964d72025
roguehashrate <roguehashrate@gmail.com>
2025-08-12 02:33:30 -0500

    Removed affiliate links because i felt scummy for it

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
         <ul><li><a href="/">Home</a></li></ul>
         <ul><li><a href="pages/project.html">Projects</a></li></ul>
         <ul><li><a href="pages/contact.html">Contact</a></li></ul>
-        <ul><li><a href="pages/affiliate.html">Affiliate</a></li></ul>
       </div>
     </div>
   `;
